754a231a7fe40d9ade7693aaa29b4cabbb251b75
G386322023 <238632@st.takushoku-u.ac.jp>
2024-07-04 16:56:19 +0900

    資料ダウンロードとリンク全部貼りと数当て4-1の途中

--- kazuate/kazuate.js
+++ kazuate/kazuate.js
@@ -17,9 +17,34 @@ hantei();
 function hantei() {
   // 将来ここでは 4 ではなくテキストボックスに指定された数値を yoso に代入する
   let yoso = 4;
-  
+
   // 課題3-1: 正解判定する
- 
+  kaisu = kaisu+1;
+
+  console.log(kaisu + '回目の予想：'+ yoso);
+
+  if(yoso == kotae || kaisu >= 4){
+    console.log('答えは　' + kotae + '　でした。すでにゲームは終わっています。');
+  }else if(yoso == kotae){
+    console.log(' 正解です。おめでとう！');
+  }else if(kaisu == 3){
+    console.log(' まちがい。残念でした答えは　' + kotae + '　です。');
+  }else if(yoso < kotae){
+    console.log(' まちがい。答えはもっと大きいですよ。');
+  }else if(yoso > kotae){
+    console.log(' まちがい。答えはもっと小さいですよ。');
+  }
+
   // kotae と yoso が一致するかどうか調べて結果を出力
   // 課題3-1における出力先はコンソール
 }
+
+//予想のメッセージ「◯回目の予想: △」を console.log() ではなく HTML に出力
+let p1 = document.querySelector('span#kaisu');			// span#kaisu 要素を検索
+p1.textContent = kaisu;	
+
+let p2 = document.querySelector('span#kaisu');			// span#answer 要素を検索
+p2.textContent = yoso;	
+
+let p2 = document.querySelector('p#result');			// p#result 要素を検索
+p2.textContent = yoso;	
