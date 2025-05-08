322d8341f792c938c7805e45a5acfddf9532e4e4
G386322023 <238632@st.takushoku-u.ac.jp>
2025-05-08 17:30:07 +0900

    kazuateフォーマット少し付け足し・プロジェクト進めた・index第3回書いた

--- kazuate.js
+++ kazuate.js
@@ -9,11 +9,14 @@ let kaisu = 0;
 // 将来以下の hantei(); の4回の呼び出しを全て削除する
 // 代わりにここでは，ボタンを押したら hantei() を呼び出すイベント処理をする
 hantei();
+hantei();
+hantei();
+hantei();
 
 // ボタンを押した後の処理をする関数 hantei() の定義
 function hantei() {
   // 将来ここでは 4 ではなくテキストボックスに指定された数値を yoso に代入する
-  yoso = 4;
+  let yoso = 4;
 
   // 課題3-1: 正解判定する
   kaisu = kaisu+1;
@@ -36,12 +39,3 @@ function hantei() {
   // 課題3-1における出力先はコンソール
 }
 
-//予想のメッセージ「◯回目の予想: △」を console.log() ではなく HTML に出力
-let p1 = document.querySelector('span#kaisu');			// span#kaisu 要素を検索
-p1.textContent = kaisu;	
-
-let p2 = document.querySelector('span#answer');			// span#answer 要素を検索
-p2.textContent = yoso;	
-
-let p3 = document.querySelector('p#result');			// p#result 要素を検索
-p3.textContent = '答えは　' + kotae + '　でした。すでにゲームは終わっています。';	
